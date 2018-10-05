19c252f84b0fd8eef89cd26a61fd06a63a4990ca
Nanthini853 <43056514+Nanthini853@users.noreply.github.com>
2018-10-05 12:41:24 +0530

    Update oddoreven.java

--- oddoreven.java
+++ oddoreven.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class Main{
+      int num=5;
 public static void main(String[] args) {
       Scanner reader = new Scanner(System.in);System.out.print("Enter a number: ");
         int num = reader.nextInt();
